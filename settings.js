1a58b53227be489613e21f17795651011ede9004
moviz509 <movizsalman@gmail.com>
2025-05-08 15:33:53 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'moviz salman', // Your name
+  botOwner: 'Knight Bot', // Your name
   ownerNumber: '923104535004', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
